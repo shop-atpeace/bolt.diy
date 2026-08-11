7b198127a148c71298e0f1a4a2f8e391cf92a653
shop-atpeace <shop.atpeacee@gmail.com>
2026-08-11 05:33:07 -0400

    Update entry.server.tsx

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,4 +1,4 @@
-export const config = { runtime: 'edge' };
+export const runtime = "edge";
 import type { AppLoadContext } from '@remix-run/cloudflare';
 import { RemixServer } from '@remix-run/react';
 import { isbot } from 'isbot';
